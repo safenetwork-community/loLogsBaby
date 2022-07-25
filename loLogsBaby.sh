62f151c92dc0b95330235c8438683620a8ab9bda
Southside <293741+willief@users.noreply.github.com>
2022-07-25 16:08:12 +0100

    Update loLogsBaby.sh
    Remove spurious / in front of $HOME, specify only genesis and node16 logs for vdash

--- loLogsBaby.sh
+++ loLogsBaby.sh
@@ -15,7 +15,7 @@ echo "Cleaning up from previous runs can take a while........"
 echo ""
 echo ""
 sleep 2
-time rm -rf /$HOME/.safe/node/baby-fleming-nodes 
+time rm -rf $HOME/.safe/node/baby-fleming-nodes 
 
 SAFE_PORT_BASE=12000
 
@@ -26,7 +26,7 @@ echo "----------------------------------------------"
 echo ""
 echo ""
 
-RUST_LOG=trace /$HOME/.safe/node/sn_node\
+RUST_LOG=trace $HOME/.safe/node/sn_node\
     -vv\
     --skip-auto-port-forwarding\
     --local-addr 127.0.0.1:$SAFE_PORT_BASE\
@@ -44,11 +44,11 @@ for  node in {2..15}
 do
     SAFE_PORT=$((SAFE_PORT_BASE + $node))
     echo "Node "$node
-    /$HOME/.safe/node/sn_node\
+    $HOME/.safe/node/sn_node\
     --skip-auto-port-forwarding \
     --local-addr 127.0.0.1:$SAFE_PORT \
-    --root-dir /$HOME/.safe/node/baby-fleming-nodes/sn-node-$node \
-    --log-dir /$HOME/.safe/node/baby-fleming-nodes/sn-node-$node &
+    --root-dir $HOME/.safe/node/baby-fleming-nodes/sn-node-$node \
+    --log-dir $HOME/.safe/node/baby-fleming-nodes/sn-node-$node &
 
     sleep 6     
     echo ""
@@ -64,7 +64,7 @@ echo "Add a final node with trace logging"
 echo ""
 echo ""
 
-RUST_LOG=trace /$HOME/.safe/node/sn_node\
+RUST_LOG=trace $HOME/.safe/node/sn_node\
     -vv\
     --skip-auto-port-forwarding\
     --local-addr 127.0.0.1:12016\
@@ -84,4 +84,7 @@ echo ""
 sleep 20
 clear    
 
-vdash $HOME/.safe/node/baby-fleming-nodes/sn-node-*/sn_node.log 
+#Ensure vdash starts with only genesis and node16 logs
+vdash\
+    $HOME/.safe/node/baby-fleming-nodes/sn-node-genesis/sn_node.log \
+    $HOME/.safe/node/baby-fleming-nodes/sn-node-16/sn_node.log 
